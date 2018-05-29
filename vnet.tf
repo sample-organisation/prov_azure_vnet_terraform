7fb51a46a3654eb93384a810e3245466f589df85
Vishnu Bharathi <vishnubharathi04@gmail.com>
2018-05-29 11:51:57 +0530

    Adds vnet name and changes var names

--- vnet.tf
+++ vnet.tf
@@ -25,15 +25,15 @@ resource "azurerm_virtual_network" "vnet_name" {
   }
 }
 
-resource "azurerm_subnet" "vnet_demo_subnet" {
-    name                 = "vnet_demo_subnet"
+resource "azurerm_subnet" "demo_subnet_tf" {
+    name                 = "demo_subnet_tf"
     resource_group_name  = "${azurerm_resource_group.resource_group_name.name}"
     virtual_network_name = "${azurerm_virtual_network.vnet_name.name}"
     address_prefix       = "10.0.2.0/24"
 }
 
-resource "azurerm_public_ip" "demo_public_ip" {
-    name                         = "demo_public_ip"
+resource "azurerm_public_ip" "demo_public_ip_tf" {
+    name                         = "demo_public_ip_tf"
     location                     = "eastus"
     resource_group_name          = "${azurerm_resource_group.resource_group_name.name}"
     public_ip_address_allocation = "dynamic"
@@ -43,8 +43,8 @@ resource "azurerm_public_ip" "demo_public_ip" {
     }
 }
 
-resource "azurerm_network_security_group" "demo_public_nsg" {
-    name                = "demo_public_nsg"
+resource "azurerm_network_security_group" "demo_public_nsg_tf" {
+    name                = "demo_public_nsg_tf"
     location            = "eastus"
     resource_group_name = "${azurerm_resource_group.resource_group_name.name}"
 
@@ -65,16 +65,16 @@ resource "azurerm_network_security_group" "demo_public_nsg" {
     }
 }
 
-resource "azurerm_network_interface" "demo_nic" {
-    name                = "demo_nic"
+resource "azurerm_network_interface" "demo_nic_tf" {
+    name                = "demo_nic_tf"
     location            = "eastus"
     resource_group_name = "${azurerm_resource_group.resource_group_name.name}"
 
     ip_configuration {
         name                          = "demo_ip_configuration"
-        subnet_id                     = "${azurerm_subnet.vnet_demo_subnet.id}"
+        subnet_id                     = "${azurerm_subnet.demo_subnet_tf.id}"
         private_ip_address_allocation = "dynamic"
-        public_ip_address_id          = "${azurerm_public_ip.demo_public_ip.id}"
+        public_ip_address_id          = "${azurerm_public_ip.demo_public_ip_tf.id}"
     }
 
     tags {
@@ -93,7 +93,7 @@ resource "random_id" "randomId" {
 }
 
 # Create a storage account to store the boot diagnostics for a VM
-resource "azurerm_storage_account" "demo_storage_account" {
+resource "azurerm_storage_account" "demo_storage_account_tf" {
     name                = "diag${random_id.randomId.hex}"
     resource_group_name = "${azurerm_resource_group.resource_group_name.name}"
     location            = "eastus"
